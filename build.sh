fcd0e3b680d3565ba6640edb6230711b422a05a6
baokhuong0507 <baokhuong.0507@gmail.com>
2025-08-14 14:06:47 +0700

    Mô tả thay đổi của bạn

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-# Thoát ngay nếu có lỗi
+# Dừng ngay lập tức nếu có lỗi
 set -o errexit
 
-# Cài đặt các thư viện
+# Nâng cấp pip và cài đặt các gói từ requirements.txt
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Chạy lệnh Python để tạo các bảng trong CSDL dựa trên models.py
+# Chạy lệnh để tạo các bảng trong CSDL
 python -c "from database import Base, engine; Base.metadata.create_all(bind=engine)"
